ea989a8f89991a48150ccf122171f342aaa247d9
Eugene Ostashko <ostashko4@gmail.com>
2019-10-13 22:33:38 +0300

    Fix error commit

--- lib/storage/storage_manager.dart
+++ lib/storage/storage_manager.dart
@@ -4,14 +4,14 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class StorageManager {
-  static final secureStorage = FlutterSecureStorage();
+  static final _secureStorage = FlutterSecureStorage();
 
   static Future<void> saveUser({@required String accessToken}) async {
-    await secureStorage.write(key: accessTokenKey, value: accessToken);
+    await _secureStorage.write(key: accessTokenKey, value: accessToken);
   }
 
   static Future<String> getAccessToken() async {
-    return await secureStorage.read(key: accessTokenKey);
+    return await _secureStorage.read(key: accessTokenKey);
   }
 
   static Future<bool> isFirstLaunch() async {
@@ -24,17 +24,17 @@ class StorageManager {
   }
 
   static Future<bool> isLoggedIn() async {
-    String accessToken = await secureStorage.read(key: accessTokenKey);
+    String accessToken = await _secureStorage.read(key: accessTokenKey);
     return accessToken != null;
   }
 
   static Future<void> deleteToken() async {
-    await secureStorage.delete(key: accessTokenKey);
+    await _secureStorage.delete(key: accessTokenKey);
   }
 
   static Future<void> clear() async {
     final pref = await SharedPreferences.getInstance();
     await pref.clear();
-    await secureStorage.deleteAll();
+    await _secureStorage.deleteAll();
   }
 }
